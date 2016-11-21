265caf0676d449ea40ad61520dd3dd3e2504f66d
Cracked Jar <oppewala@gmail.com>
2016-11-21 18:17:05 +1100

    Add project files.

---
+++ src/CraftOrBuy/Classes/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftOrBuy.Classes
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public bool IsAuctionable { get; set; }
+        public ItemBind ItemBind { get; set; }
+        public ItemClass ItemClass { get; set; }
+        public ItemSubClass ItemSubClass { get; set; }
+    }
+
+    public enum ItemBind
+    {
+
+    }
+
+    public enum ItemClass
+    {
+
+    }
+
+    public enum ItemSubClass
+    {
+
+    }
+}
---
+++ src/CraftOrBuy/LogicLayer/Recipes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CraftOrBuy.Classes;
+
+namespace CraftOrBuy.LogicLayer
+{
+    public class Recipes
+    {
+        List<Recipe> recipes = new List<Recipe>();
+
+        public IEnumerable<Recipe> RetrieveRecipes()
+        {
+            recipes.Add(new Recipe
+            {
+                Name = "Rec",
+                ID = 123,
+                Ingredients = new Dictionary<Item, int>
+                {
+                    { new Item { ID = 123145 }, 5 },
+                    { new Item { ID = 123146 }, 5 },
+                    { new Item { ID = 123147 }, 2 }
+                }
+            });
+
+            return recipes;
+        }
+    }
+}
